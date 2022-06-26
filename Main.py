84c88165b238a1cc52f718bf6fd4207abb8167f5
TsyhanouMikhail <v.i.p.misha@mail.ru>
2022-06-26 17:57:39 +0300

    formula + osibka new

--- Main.py
+++ Main.py
@@ -1,7 +1,15 @@
 print("hello word")
+print("hello word")
+print("hello word")
+print("hello word")
+print("hello word")
+    print("hello word")
+        print("hello word")
+print("hello word")
 a = 8
 print(a)
 
 print("hello word")
 a = 99
 print(a)
+print(b)
