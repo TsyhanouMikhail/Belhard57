786b9789035379bb24865e2d9be7200d593afd7c
TsyhanouMikhail <v.i.p.misha@mail.ru>
2022-06-26 18:58:07 +0300

    home work2

--- Main.py
+++ Main.py
@@ -1,15 +1,1 @@
 print("hello word")
-print("hello word")
-print("hello word")
-print("hello word")
-print("hello word")
-    print("hello word")
-        print("hello word")
-print("hello word")
-a = 8
-print(a)
-
-print("hello word")
-a = 99
-print(a)
-print(b)
