a0a76d8280e90881d080ecf9f52560734287ae13
TsyhanouMikhail <v.i.p.misha@mail.ru>
2022-07-03 13:05:06 +0300

    new version. the best

--- 3.4.py
+++ 3.4.py
@@ -36,6 +36,23 @@ elif float(number3) < 0:
 elif float(number3) == 0:
     formula3 = formula3 + 1
 
+
 print("Положительных чисел: ", formula1)
 print("Отрицательных чисел: ", formula2)
 print("Нулей: ", formula3)
+
+# вариант 2 проще
+
+a: int = int(input("Введите число 1: "))
+b: int = int(input("Введите число 2: "))
+c: int = int(input("Введите число 3: "))
+
+print("Вы ввели числа: ", (a), (b), (c))
+
+positive_count = (a > 0) + (b > 0) + (c > 0)
+negative_count = (a < 0) + (b < 0) + (c < 0)
+zero_count = (a == 0) + (b == 0) + (c == 0)
+
+print("Положительных чисел: ", f"{positive_count=}")
+print("Отрицательных чисел: ", f"{negative_count=}")
+print("Нулей: ", f"{zero_count=}")
