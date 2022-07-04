b1d7a86a3c4c0ac767c834fb7402b1f43ed547c4
TsyhanouMikhail <v.i.p.misha@mail.ru>
2022-07-05 00:34:46 +0300

    na proverku new

--- 4.2.py
+++ 4.2.py
@@ -1,26 +1,17 @@
 # Без использования collections, написать программу, которая будет
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
-from typing import List
 
 data = {
     "fname": str(input("Введите имя: ")),
     "lname": str(input("Введите фамилию: "))
 
 }
-#print(data)
 
 text = list(data.values())
-print(text)
-letters: List[str] = list(text)
 
-# не работает разбивка на символы. может опять какой глюк. с Хелло ворд все работает.
-# или недостаток серого вещества :-(
+text: str = "".join(text)
 
-# text = "hello world"
-# print(text)
-# letters: List[str] = list(text)
-# print(letters)
+letters: str = list(text)
 
-print(letters)
 print(letters.count(input("Введите символ, который необходимо искать: ")))
